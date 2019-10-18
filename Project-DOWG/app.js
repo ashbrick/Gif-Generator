00eb9eb98b88d862c3b21ad38591a617928b5124
ashbrick <ashleycbrickhouse@gmail.com>
2019-10-18 16:17:03 -0400

    adding initial file setup for project 1

---
+++ Project-DOWG/app.js
@@ -0,0 +1,15 @@
+/*
+
+const api = "http://api.giphy.com/v1/gifs/search?" //url to api
+
+const apiKey = "&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal
+
+var query = "&q=coffee"
+
+search query path: /v1/gifs/search
+
+q=coffee
+
+*/
+
+http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee
