477b6ef74b3ed9c3b87612aac59d7d3bd068c756
ashbrick <ashleycbrickhouse@gmail.com>
2019-10-19 12:00:37 -0400

    DOM populated with specific data needed, including title, url, and gif rating

--- Project-DOWG/app.js
+++ Project-DOWG/app.js
@@ -5,8 +5,12 @@ $(()=>{
         url: "http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee",
     }).then(
         (data)=>{
-            data.data[0];
-            console.log(data.data[0])
+            data.data[0].title;
+            data.data[0].images.original.url;
+            data.data[0].rating;
+            // console.log(data.data[0].title)
+            // console.log(data)
+            console.log(data.data[0].rating)
         },
         (error)=>{
             console.log('Error');
@@ -44,5 +48,28 @@ q=coffee
 url= api + apiKey + query
 http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee
 
+//-->experimenting with different keywords to see if I can get the data to populate
+
+1. try coffee and tired
+
+
 ////////////////////////////////////////////////////////////////////
+*/
+
+/*=============================================
+              FUNCTIONALITY NOTES
+=============================================
+
+-when user enters day of the week and clicks the FIND GIFS button, the carousel will display 25 gifs based on the keywords for that day of the week
+
+Keywords
+Monday:
+Tuesday:
+Wednesday:
+Thursday:
+Friday:
+Saturday:
+Sunday:
+
+
 */
