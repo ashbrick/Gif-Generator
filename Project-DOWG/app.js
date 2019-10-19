bc5341ea3696a15e7d234881d890585da0fc4653
ashbrick <ashleycbrickhouse@gmail.com>
2019-10-18 22:26:59 -0400

    created HTML

--- Project-DOWG/app.js
+++ Project-DOWG/app.js
@@ -1,4 +1,34 @@
-/*
+
+$(()=>{
+
+    $.ajax({ //need to add the variables for query, user input, figure out others
+        url: "http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee",
+    }).then(
+        (data)=>{
+            //data to query goes here
+        },
+        (error)=>{
+            console.log('Error');
+        }
+    );
+
+
+
+
+
+
+
+
+
+})
+
+
+
+/* ============================================
+                        AJAX NOTES
+===============================================
+
+/////////////// GENERAL INFO & testing//////////////////////////
 
 const api = "http://api.giphy.com/v1/gifs/search?" //url to api
 
@@ -10,6 +40,7 @@ search query path: /v1/gifs/search
 
 q=coffee
 
-*/
-
 http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee
+
+////////////////////////////////////////////////////////////////////
+*/
