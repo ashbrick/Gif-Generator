64fc8984d0d57ebffe4951cc71c70a1af0be0075
ashbrick <ashleycbrickhouse@gmail.com>
2019-10-19 20:07:39 -0400

    find gifs button works

--- Project-DOWG/app.js
+++ Project-DOWG/app.js
@@ -1,6 +1,26 @@
 
 $(()=>{
 
+//===============================================
+//                 VARIABLES
+//===============================================
+
+
+//===============================================
+//                  FUNCTIONS
+//===============================================
+
+    $('form').on('click', (event) =>{
+        const $userInput = $('input[type="text"]').val();
+        console.log($userInput)
+        event.preventDefault();
+        $('form').trigger('reset');
+    })
+
+//===============================================
+//                  AJAX & DATA
+//===============================================
+
     $.ajax({ //need to add the variables for query, user input, figure out others
         url: "http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee",
     }).then(
@@ -30,7 +50,9 @@ $(()=>{
 
 
 /* ============================================
+===============================================
                         AJAX NOTES
+===============================================
 ===============================================
 
 /////////////// GENERAL INFO & testing//////////////////////////
@@ -48,28 +70,41 @@ q=coffee
 url= api + apiKey + query
 http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee
 
-//-->experimenting with different keywords to see if I can get the data to populate
+//-->experimenting with different keywords to see if I can get the data to populate and trying to decide how I want to filter the search for each day while keeping it simple
 
 1. try coffee and tired
+http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee+tired
+-total count returned: 13,360
 
+2. try coffe, tired, grumpy motivation
+http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee+tired+grumpy+motivation
+-total count returned: 17,288
+
+3. try coffee
+http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee
+-total count: 16,298
 
 ////////////////////////////////////////////////////////////////////
 */
 
+
+
 /*=============================================
+===============================================
               FUNCTIONALITY NOTES
-=============================================
+===============================================
+===============================================
 
 -when user enters day of the week and clicks the FIND GIFS button, the carousel will display 25 gifs based on the keywords for that day of the week
 
 Keywords
-Monday:
-Tuesday:
-Wednesday:
-Thursday:
-Friday:
-Saturday:
-Sunday:
+Monday: coffee, tired, grumpy, motivation
+Tuesday: tacos
+Wednesday: humpday,
+Thursday: friday eve
+Friday: dance,
+Saturday: party
+Sunday: rest, sleep, relax
 
 
 */
