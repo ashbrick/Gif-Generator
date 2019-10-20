a4f89a492668e45d48bc9d357bb8a8b6a1bb521e
ashbrick <ashleycbrickhouse@gmail.com>
2019-10-20 17:56:34 -0400

    got the gifs to randomly show up one by one on the page but still can't get the gifs to correspond to my pre-selected keywords

--- Project-DOWG/app.js
+++ Project-DOWG/app.js
@@ -4,37 +4,46 @@ $(()=>{
 //===============================================
 //                 VARIABLES
 //===============================================
-    // var $dayOfWeek = ()
 
     const $userInput = $('input[type="text"]').val().toLowerCase();
 
-    const $monQuery = "coffee+tired+grumpy"
-    const $tueQuery = "tacos"
-    const $wedQuery = "humpday+wednesday"
-    const $thuQuery = "friday+eve"
-    const $friQuery = "dance"
-    const $satQuery = "party"
-    const $sunQuery = "rest+relax+sleep"
+    const chooseDay =()=>{
+            const $monQuery = "coffee+tired+grumpy";
+            const $tueQuery = "tacos";
+            const $wedQuery = "humpday+wednesday";
+            const $thuQuery = "friday+eve";
+            const $friQuery = "dance";
+            const $satQuery = "party";
+            const $sunQuery = "rest+relax+sleep";
 
+            if($userInput == 'monday'){
+                let $userInput = $monQuery;
+            } else if ($userInput == 'tuesday'){
+                let $userInput = $tueQuery;
+            } else if ($userInput == 'wednesday'){
+                let $userInput = $wedQuery;
+            } else if ($userInput == 'thursday'){
+                let $userInput = $thuQuery;
+            } else if ($userInput == 'friday'){
+                let $userInput = $friQuery;
+            } else if ($userInput == 'saturday'){
+                let $userInput = $satQuery;
+            }else if ($userInput == 'sunday'){
+                let $userInput = $sunQuery;
+            };
+    }
 //===============================================
-//                  FUNCTIONS
+//                  FUNCTIONS: for Buttons
 //===============================================
 
     $('#findGifs').on('click', (event) =>{
-        const $userInput = $('input[type="text"]').val().toLowerCase();
+        const $userInput = $('input[type="text"]').val().toLowerCase()
+        // chooseDay();
+
+
         // console.log($userInput)
         event.preventDefault();
         $('form').trigger('reset');
-
-
-        // if user input is Monday, return "this url" as the query parameters
-        const daySearch = ()=>{
-            if($userInput == monday){
-            // if($userInput == monday){
-                let $userInput = $monQuery;
-            };
-            daySearch();
-        }
 
 
 //===============================================
@@ -42,77 +51,26 @@ $(()=>{
 //===============================================
 
 /* ajaxRequest syntax learned from https://www.youtube.com/watch?v=pfdP26jPvEk */
-        var ajaxRequest = $.get(`http://api.giphy.com/v1/gifs/random?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&limit=10&q=${$userInput}`);
+        var ajaxRequest = $.get(`http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&limit=25&q=${$userInput}`);
 
         ajaxRequest.done((response)=>{
             console.log("success got data", response); //checking to see if able to access the data
 
+            let i = Math.floor(Math.random()*25)
             //set response(data) to a variable to make it easier to handle (especially because otherwise it would be data.data which can be confusing -- the response is a bunch of gifs so setting the variable as "gifs")
-
-            // const randNumGen = () =>{
-            //     for(i=0; i<3;i++){
-            //     }
-            // }
-
-            // let i = Math.floor(Math.random()*3)
-
-
             var $gifs = response.data
 
-            // let $('#carousel-list') = [];
+            $('#carousel-list').empty();
+            $('#carousel-list').append("<img src=' "+$gifs[i].images.original.url+" ' style='height: 350px; width: 350px;'/>" )
 
-
-            //create a for/in statement to loop through properties of an object (response is an object)
-            //set size for gifs inside of image source
-            for(i in $gifs){
-                $('#carousel-list').empty();
-                $('#carousel-list').append("<img src=' "+$gifs.images.original.url+" ' style='height: 350px; width: 350px;'/>" )
-            }
-
-
+            console.log(i)
 
         });
     }) //closing for on click function on line 24
 
 }) //DOM onload closing
 
-//======================== 1st attempt, couldn't get it to work so trying a different syntax
 
-    // $.ajax({ //need to add the var for userinput in place of query so that the day of the week corresponds with the search parameters
-    //     url: `http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&limit=1&q=${$userInput}`,
-    // }).then(
-    //     (data)=>{
-    //         // let $data = data.data
-    //         // $('#title').html(data.data[""].title);
-    //         // $('#url').html(data.data[0].images.original.url);
-    //         $('#url').html(data.data[0].images.original.url);
-    //         // $('#rating').html(data.data[].rating);
-    //
-    // //========== CONSOLE LOGS to test AJAX data retrieval ==========================
-    //
-    //         // console.log(data.data[0].title)
-    //         // console.log(data.data[0].images.original.url)
-    //         // console.log(data.data[0].rating)
-    //         // console.log(data.data[4].images.original.url)
-    //         // console.log(data.data[0].images)
-    //         // console.log(data.data[]) //gave me an error, didn't understand the empty brackets
-    //         // console.log(data.data) //showed nothing in console, and no errors
-    //         console.log($data)
-    //
-    //
-    //
-    //         // for(let i=0; i<data.data.length; i++) {
-    //         //     ($('<li>')).append(data.data[0]);
-    //         //     const $li = $('li').html('img src= data.data[i].embed_url')
-    //         //     $('#carousel-list').append($li)
-    //         // };
-    //
-    //
-    //     }),
-    //     (error)=>{
-    //         console.log('Error');
-    //     };
-    // })
 
 
 
@@ -160,7 +118,48 @@ http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=
 http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=coffee
 -total count: 16,298
 
-////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////
+1st ATTEMPT at creating the ajax request and grabbing the data
+/////////////////////////////
+    Note: decided to start over because I couldn't get it to work. Tried a different syntax that made more sense to me
+
+    $.ajax({ //need to add the var for userinput in place of query so that the day of the week corresponds with the search parameters
+        url: `http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&limit=1&q=${$userInput}`,
+    }).then(
+        (data)=>{
+            // let $data = data.data
+            // $('#title').html(data.data[""].title);
+            // $('#url').html(data.data[0].images.original.url);
+            $('#url').html(data.data[0].images.original.url);
+            // $('#rating').html(data.data[].rating);
+
+    //========== CONSOLE LOGS to test AJAX data retrieval ==========================
+
+            // console.log(data.data[0].title)
+            // console.log(data.data[0].images.original.url)
+            // console.log(data.data[0].rating)
+            // console.log(data.data[4].images.original.url)
+            // console.log(data.data[0].images)
+            // console.log(data.data[]) //gave me an error, didn't understand the empty brackets
+            // console.log(data.data) //showed nothing in console, and no errors
+            console.log($data)
+
+        //ended up not needing a for loop, didn't make sense for what I was trying to do. Since I could get it done without a for loop I took this out to keep the code more DRY
+            // for(let i=0; i<data.data.length; i++) {
+            //     ($('<li>')).append(data.data[0]);
+            //     const $li = $('li').html('img src= data.data[i].embed_url')
+            //     $('#carousel-list').append($li)
+            // };
+
+
+        }),
+        (error)=>{
+            console.log('Error');
+        };
+    })
+
 */
 
 
@@ -182,6 +181,76 @@ Friday: dance,
 Saturday: party
 Sunday: rest, sleep, relax
 
+/////////////////////////////// Explanation for removing daySearch function
+
+-Decided to use a switch statement for this (Thank you, Jordan Morales):
+    if user input is Monday, return "this url" as the query parameters
+    const daySearch = ()=>{
+        if($userInput == monday){
+        // if($userInput == monday){
+            let $userInput = $monQuery;
+        };
+        daySearch();
+    }
+
+    ////////////////////////// The Switch Statement Attempt
+    Note: couldn't figure out where to put it so went back to if/else
+
+    const whatDay = () =>{
+
+        const $userInput = $('input[type="text"]').val().toLowerCase();
+
+            const $monQuery = "coffee+tired+grumpy";
+            const $tueQuery = "tacos";
+            const $wedQuery = "humpday+wednesday";
+            const $thuQuery = "friday+eve";
+            const $friQuery = "dance";
+            const $satQuery = "party";
+            const $sunQuery = "rest+relax+sleep";
+
+        switch ($userInput) {
+            case 'monday':
+                $userInput = $monQuery;
+                break;
+            case 'tuesday':
+                $userInput = $tueQuery;
+                break;
+            case 'wednesday':
+                $userInput = $wedQuery;
+                break;
+            case 'thursday':
+                $userInput = $thuQuery;
+                break;
+            case 'friday':
+                $userInput = $friQuery;
+                break;
+            case 'saturday':
+                $userInput = $satQuery;
+                break;
+            case 'sunday':
+                $userInput = $sunQuery;
+                break;
+            default:
+                $userInput;
+        };
+    }
+
+/////////////////////////////// Explanation for removing my raondom num generator
+
+-took this out of my current AJAX statement because I didn't need it to generate a random number (Dan helped to clarify things for me). Instead I just kept the Math.floor(Math.random) part above in order to randomly grab a gif from the ones generated based on $userInput
+
+generate random number from gifs returned
+const randNumGen = () =>{
+    for(i=0; i<=5;i++){
+
+    }
+}
+
+//////////////////////////////// Explanation for removing my for/in loop
+
+-original pseudo-code: create a for/in statement to loop through properties of an object (response is an object)
+
+    Ended up not needing it to filter through properties since I'm randomly picking 1 gif
 
 */
 
@@ -192,7 +261,7 @@ Sunday: rest, sleep, relax
 ===============================================
 ===============================================
 
-1. Data isn't being recognized, nor is images so I'm having a hard time getting the specific data I need
+1. "Data" isn't being recognized, nor is "images" so I'm having a hard time getting the specific data I need
 
     $.ajax({ //need to add the var for userinput in place of query so that the day of the week corresponds with the search parameters
         url: `http://api.giphy.com/v1/gifs/search?&api_key=jl0KBxJT91RtqFEeeJapgitK8gMWZDal&q=monday`,
@@ -210,4 +279,9 @@ Sunday: rest, sleep, relax
                 const $li = $('li').html('img src= data.data[i].embed_url')
                 $('#carousel-list').append($li)
             };
+
+    EXPLANATION:
+        -I switched my syntax to see if it would help me to understand it better. After watching a YouTube video about the different syntax and how it was used it helped!
+
+        -I also wasn't calling the data correctly. So instead of referring to the gifs as 'img src= data.data[i].embed_url', I went deeper into the API and grabbed a different property buried within $gifs[i].images.original and got it to work FINALLY!
 */
